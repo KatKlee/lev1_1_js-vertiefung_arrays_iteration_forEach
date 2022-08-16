900f5b6dbef1d551e696061d75b8f785f7c24790
KatKlee <katklee2022@gmail.com>
2022-08-16 11:03:59 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let getraenke = ["Coca-Cola", "Apfelsaft", "Pepsi", "Traubensaft", "Sprite", "Orangensaft", "Red Bull Energy Drink", "Fanta"]
+
+getraenke.sort()
+
+let myDrinks = getraenke.forEach((item) => {
+    document.write(item + '<br>')
+})
+
+console.log(getraenke)
